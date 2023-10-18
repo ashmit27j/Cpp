3979cdc381ddb743ab461d043a429713862c7b34
Ashmit <ashmit27j@gmail.com>
2023-10-18 16:17:17 +0530

    changes

--- Interesting Codes/String/Delete_string.cpp
+++ Interesting Codes/String/Delete_string.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-    string str = "India Indie Indoi";
-    string ele = "Indie";
-}
+    string str1 = "India Indoi Indae";
+    string word = "In";
+    string newstr = "";
+    int k = 0, flag = 0;
+    for (int i = 0; i < str1.length(); i++)
+    {
+        k = 0, flag = 0;
+        for (int j = i; j < word.length() + i; j++)
+        {
+            if (str1[j] == word[k])
+            {
+                flag = 1;
+                continue;
+            }
+            else
+            {
+                break;
+            }
+            k++;
+        }
+        if (flag == 1)
+        {
+            i += word.length() - 1;
+        }
+    }
+} // namespace name
