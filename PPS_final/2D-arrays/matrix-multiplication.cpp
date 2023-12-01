c1c760b908d30416db161ccdcdffae24db03c61f
Ashmit Jain <ashmit27j@gmail.com>
2023-12-01 16:31:16 +0530

    fixed: - matrix multiplication

--- PPS_final/2D-arrays/matrix-multiplication.cpp
+++ PPS_final/2D-arrays/matrix-multiplication.cpp
@@ -2,39 +2,57 @@
 using namespace std;
 int main()
 {
-    int m1, n1, m2, n2;
-    cout << "Enter row x column for arr1";
-    cin >> m1 >> n1;
-    cout << "Enter row x column for arr2";
-    cin >> m2 >> n2;
-    int arr1[m1][n1];
-    int arr2[m2][n2];
-    cout << "Enter array 1";
-    for (int i = 0; i < m1; i ++ )
+    int r1, c1, r2, c2;
+    cout << "Enter row x column for arr1\n";
+    cin >> r1 >> c1;
+    cout << "Enter row x column for arr2\n";
+    cin >> r2 >> c2;
+    int arr1[r1][c1];
+    int arr2[r2][c2];
+    cout << "Enter array 1\n";
+    for (int i = 0; i < r1; i ++ )
     {
-        for (int j = 0; j < n1; j ++ )
+        for (int j = 0; j < c1; j ++ )
         {
             cin >> arr1[i][j];
         }
     }
-    cout << "Enter array 2";
-    for (int i = 0; i < m2; i ++ )
+    cout << "Enter array 2\n";
+    for (int i = 0; i < r2; i ++ )
     {
-        for (int j = 0; j < n2; j ++ )
+        for (int j = 0; j < c2; j ++ )
         {
             cin >> arr2[i][j];
         }
-        cout << endl;
     }
-    cout << "sum of arrays given: \n";
-    if ()
-    for (int i = 0; i < m; i ++ )
+    int mult[r1][c2];
+    //row of the first guy and col of the second array
+    if (c1 == r2 )
     {
-        for (int j = 0; j < m; j ++ )
+        
+        for (int i = 0; i < r1; i ++ )
         {
-            for (int k = 0; k < )
+            for (int j = 0; j < c2; j ++ )
+            {
+                mult[i][j] = 0;
+                for (int k = 0; k < c1; k++ )
+                {
+                    mult[i][j] += ( arr1[i][k] * arr2[k][j] );
+                }
+            }
+        }
+    }
+    else
+    {
+        cout << "Invalid matrices!";
+        return 0;
+    }
+    for (int i = 0; i < r1; i ++ )
+    {
+        for (int j = 0; j < c2; j ++ )
+        {
+           cout << mult[i][j] << " ";
         }
         cout << endl;
     }
-    
 }
